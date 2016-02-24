7f7fe6d31b4eb8326c5e14e9f40b46e015c1f89f
Acacia Mack <acacialmack@gmail.com>
2016-02-24 07:08:38 -0500

    able to add car images

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,8 +1,8 @@
 class Car < ActiveRecord::Base
-  validates :make, presence: true, uniqueness: true
-  validates :model, presence: true, uniqueness: true
-  validates :year, presence: true, uniqueness: true
-  validates :price, presence: true
+  # validates :make, presence: true, uniqueness: true
+  # validates :model, presence: true, uniqueness: true
+  # validates :year, presence: true, uniqueness: true
+  # validates :price, presence: true
 
   belongs_to :dealer
   has_and_belongs_to_many :users
